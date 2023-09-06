16e73a298e760f1abe118c6998a13e1cf95d7b8f
g0-0py <brendan@bsherry.io>
2023-09-06 15:14:26 -0500

    Classes Part 2 Exercises Complete
    Signed-off-by: g0-0py <brendan@bsherry.io>

--- classes-part-2/exercises/src/main/java/org/org/launchcode/Course.java
+++ classes-part-2/exercises/src/main/java/org/org/launchcode/Course.java
@@ -1,5 +1,4 @@
 package org.launchcode;
-
 import java.util.ArrayList;
 
 public class Course {
--- classes-part-2/exercises/src/main/java/org/org/launchcode/Student.java
+++ classes-part-2/exercises/src/main/java/org/org/launchcode/Student.java
@@ -30,13 +30,25 @@ public class Student {
 
 
     //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+    public static String getGradeLevel(int credits) {
+
+        if(credits <= 29) {
+            return "freshman";
+        } else if (credits <= 59) {
+            return "sophomore";
+        } else if (credits <= 89) {
+            return "junior";
+        } else {
+            return "senior";
+        }
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
-        // Update the appropriate fields: numberOfCredits, gpa
+        double totalQualityScore = this.gpa * this.numberOfCredits;
+        totalQualityScore += courseCredits * grade;
+        this.numberOfCredits += courseCredits;
+        this.gpa = totalQualityScore / this.numberOfCredits;
     }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
@@ -75,6 +87,35 @@ public class Student {
 
     private void setNumberOfCredits(int numberOfCredits) {
         this.numberOfCredits = numberOfCredits;
+    }
+
+    public String toString() {
+        String studentReport = String.format("%s is a %s with %d credits and a GPA of %.2f",
+                this.name,
+                this.getGradeLevel(this.numberOfCredits),
+                this.getNumberOfCredits(),
+                this.getGpa());
+
+        return studentReport;
+    }
+
+    public boolean equals (Object toBeCompared) {
+
+        if (toBeCompared == this) {
+            return true;
+        }
+
+        if (toBeCompared == null) {
+            return false;
+        }
+
+        if (toBeCompared.getClass() != getClass()) {
+            return false;
+        }
+
+        Student theStudent = (Student) toBeCompared;
+        return theStudent.getStudentId() == getStudentId();
+
     }
 
     public static void main(String[] args) {
